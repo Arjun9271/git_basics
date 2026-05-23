6f8610db8e82825cdb73f30823916587dba7c8f8
Arjun9271 <arjun.t1729@gmail.com>
2026-05-23 20:28:00 +0530

    python oops code

---
+++ app.py
@@ -0,0 +1,10 @@
+class Students:
+    def __init__(self,name,s_class,roll_number):
+        self.name = name
+        self.s_class = s_class
+        self.roll_number = roll_number
+        
+        
+        
+student_1 = Students("ram","10th",1025)
+print(student_1.name)
