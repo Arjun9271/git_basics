23f1446493a5741a566fdfaac8ea10f801b31936
Arjun9271 <arjun.t1729@gmail.com>
2026-05-23 20:38:05 +0530

    section removed

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 class Students:
-    def __init__(self,name,s_class,roll_number,section):
+    def __init__(self,name,s_class,roll_number):
         self.name = name
         self.s_class = s_class
         self.roll_number = roll_number
-        self.section = section
+      
         
         
         
-student_1 = Students("ram","10th",1025,'B')
-student_2 = Students('arjun','10th',72,'A')
+student_1 = Students("ram","10th",1025)
+student_2 = Students('arjun','10th',72)
 
 print(student_1.name)
